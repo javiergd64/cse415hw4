e095aef86a30437ad6b153f5e2207734dd4354b8
Javier Guapilla-Diaz <javiergd@uw.edu>
2025-11-13 12:11:44 -0800

    - added/fixed some imports - added globval GAME_TYPE variable - tested and passed local test for make_move -> based heavily on RandomPlayer file we got. NOTE: i kept the old RandomPlayer stuff under our make move COPIED from RandomPlayer: - chooseMove(): used it test for now (since no minimax or ab yet) - move_gen(): helper for successors_and_moves - successors_and_moves(): to generate successors - do_move(): actually makes the move

--- a4-starter-code/yourUWNetID_KInARow.py
+++ a4-starter-code/yourUWNetID_KInARow.py
@@ -11,6 +11,12 @@ from mpmath.functions.zetazeros import count_to
 
 from agent_base import KAgent
 from game_types import State, Game_Type
+# import game_types # idk why it didnt work, but changed it to random player import
+# ^NOTE, it didnt work b/c when I copied make_move() from random_player,
+# I forgot to change its argument of:
+# news = game_types.State to just State
+from random import randint # for the random choosing (before mM or ab)
+GAME_TYPE = None
 
 AUTHORS = 'Javier Guapilla-Diaz; Ivonne Zhang'
 UWNETIDS = ['javiergd', 'yimenz5'] # The first UWNetID here should
@@ -63,38 +69,80 @@ class OurAgent(KAgent):  # Keep the class name "OurAgent" so a game master
                                       # or something simple and quick to compute
                                       # and do not import any LLM or special APIs.
                                       # During the tournament, this will be False..
-       if utterances_matter:
-           pass
-           # Optionally, import your LLM API here.
-           # Then you can use it to help create utterances.
+        if utterances_matter:
+            pass
+            # Optionally, import your LLM API here.
+            # Then you can use it to help create utterances.
            
-       # Write code to save the relevant information in variables
-       # local to this instance of the agent.
-       # Game-type info can be in global variables.
-       print("Change this to return 'OK' when ready to test the method.")
-       return "Not-OK"
-   
+        # Write code to save the relevant information in variables
+        # local to this instance of the agent.
+        # Game-type info can be in global variables.
+
+        ## -- i got this from random player to set up variables and stuff
+        # I realized it was missing when the test wasnt working on the game master :,)
+        self.who_i_play = what_side_to_play
+        self.opponent_nickname = opponent_nickname
+        self.time_limit = expected_time_per_move
+        global GAME_TYPE
+        GAME_TYPE = game_type
+        print("Oh, I love playing randomly at ", game_type.long_name)
+        self.my_past_utterances = []
+        self.opponent_past_utterances = []
+        self.repeat_count = 0
+        self.utt_count = 0
+        if self.twin: self.utt_count = 5  # Offset the twin's utterances.
+
+       #print("Change this to return 'OK' when ready to test the method.")
+        return "OK" #"Not-OK"
+
+    # working on this first -j
+    # note for ivonne: I used the random player for layout and initial
+    # "test" of randomness
     # The core of your agent's ability should be implemented here:             
     def make_move(self, current_state, current_remark, time_limit=1000,
-                  use_alpha_beta=True,
+                  use_alpha_beta=False, #True, uncomment for later
                   use_zobrist_hashing=False, max_ply=3,
                   special_static_eval_fn=None):
-        print("make_move has been called")
 
-        print("code to compute a good move should go here.")
-        # Here's a placeholder:
-        a_default_move = (0, 0) # This might be legal ONCE in a game,
-        # if the square is not forbidden or already occupied.
-    
-        new_state = current_state # This is not allowed, and even if
-        # it were allowed, the newState should be a deep COPY of the old.
-    
-        new_remark = "I need to think of something appropriate.\n" +\
-        "Well, I guess I can say that this move is probably illegal."
+        # a list of places we can go, and the move associated
+        possible_S, possible_M = successors_and_moves(current_state)
+        # S is state and M is move
+        
+        # successors empty = can't go anywhere, so cant do anything
+        if (len(possible_S) == 0):
+            utter = "Welp, I'm cornered"
+            return [[None, current_state], utter]
+        
+        # this is to test w/o minimax or ab
+        
+        new_S, new_M = chooseMove((possible_S, possible_M))
+        
+        utter = "TEST UTTERANCE" # for now cuz we will use llm?
 
-        print("Returning from make_move")
-        return [[a_default_move, new_state], new_remark]
+        return [[new_M, new_S], utter]
 
+        # --- OLD ---
+        # Kept it here in case we need to look over it or revert something
+
+        # print("make_move has been called")
+        #
+        # print("code to compute a good move should go here.")
+        # # Here's a placeholder:
+        # a_default_move = (0, 0) # This might be legal ONCE in a game,
+        # # if the square is not forbidden or already occupied.
+        #
+        # new_state = current_state # This is not allowed, and even if
+        # # it were allowed, the newState should be a deep COPY of the old.
+        #
+        # new_remark = "I need to think of something appropriate.\n" +\
+        # "Well, I guess I can say that this move is probably illegal."
+        #
+        # print("Returning from make_move")
+        # return [[a_default_move, new_state], new_remark]
+
+
+    # next chunk
+    # -------------------------------------------------------------------- #
     # The main adversarial search function:
     def minimax(self,
             state,
@@ -185,6 +233,54 @@ class OurAgent(KAgent):  # Keep the class name "OurAgent" so a game master
         return score
  
 # OPTIONAL THINGS TO KEEP TRACK OF:
+
+# THESE I GOT FROM RANDOM PLAYER (they seemed useful):
+def other(p):
+    if p=='X': return 'O'
+    return 'X'
+
+# Randomly choose a move.
+def chooseMove(statesAndMoves):
+    states, moves = statesAndMoves
+    if states==[]: return None
+    random_index = randint(0, len(states)-1)
+    my_choice = [states[random_index], moves[random_index]]
+    return my_choice
+
+# The following is a Python "generator" function that creates an
+# iterator to provide one move and new state at a time.
+# It could be used in a smarter agent to only generate SOME of
+# of the possible moves, especially if an alpha cutoff or beta
+# cutoff determines that no more moves from this state are needed.
+def move_gen(state):
+    b = state.board
+    p = state.whose_move
+    o = other(p)
+    mCols = len(b[0])
+    nRows = len(b)
+
+    for i in range(nRows):
+        for j in range(mCols):
+            if b[i][j] != ' ': continue
+            news = do_move(state, i, j, o)
+            yield [(i, j), news]
+
+# This uses the generator to get all the successors.
+def successors_and_moves(state):
+    moves = []
+    new_states = []
+    for item in move_gen(state):
+        moves.append(item[0])
+        new_states.append(item[1])
+    return [new_states, moves]
+
+# Performa a move to get a new state.
+def do_move(state, i, j, o):
+            #news = game_types.State(old=state)
+            news = State(old=state)
+            news.board[i][j] = state.whose_move
+            news.whose_move = o
+            return news
 
 #  WHO_MY_OPPONENT_PLAYS = other(WHO_I_PLAY)
 #  MY_PAST_UTTERANCES = []
