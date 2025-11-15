f15eef743315631cf97a149128cab1e6efe14824
Javier Guapilla-Diaz <javiergd@uw.edu>
2025-11-14 20:55:08 -0800

    Game Master Updated to play 5inRow,7x7 no corners.

--- a4-starter-code/Game_Master_Offline.py
+++ a4-starter-code/Game_Master_Offline.py
@@ -178,8 +178,8 @@ def test():
     # Stand-alone test
     print("Starting stand-alone test of GameMaster.py")
     # Edit this to change what version of K-in-a-Row is used.
-    set_game(TTT) # default is Tic-Tac-Toe
-    #set_game(FIAR) # Five in a Row
+    #set_game(TTT) # default is Tic-Tac-Toe
+    set_game(FIAR) # Five in a Row
     # Import 1 or 2 agent files here.
     # If using only 1, create 2 instances of it, one of
     # which is a "twin".
@@ -189,7 +189,7 @@ def test():
     import ykhan04_KInARow as h3
     px = h.OurAgent()
     #po = h.OurAgent(twin=True)
-    po = h.OurAgent(twin=True) # using the random agent
+    po = h3.OurAgent() # using the random agent
     set_players(px, po)
     print("Players are set.")
     print("Now let's run the game.")
